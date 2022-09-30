784be51c9872d22c88deac3df464d893e1ea798a
Noah Lundquist <nalundquist@gmail.com>
2022-09-30 11:13:08 -0700

    add base controller functions

--- PierreVandO/Controllers/OrdersController.cs
+++ PierreVandO/Controllers/OrdersController.cs
@@ -10,5 +10,23 @@ namespace PierreVandO.Controllers
 			List<Order> orderList = Orders.GetOrders();
 			return View(orderList);
 		}
+
+		[HttpGet("/vendors/{vendorId}/orders/new")]
+		public ActionResult New(int vendorId)
+		{
+			Vendor vendor = Vendor.FindVendor(id);
+			return View(vendor);
+		}
+
+		[HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+		public ActionResult Show(int vendorId, int orderId)
+		{
+			Order order = Order.FindOrder(orderId);
+			Vendor vendor = Vendor.FindVendor(vendorId);
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			model.Add("order", order);
+			model.Add("vendor", vendor);
+			return View(model);
+		}
 	}
 }
--- PierreVandO/Controllers/VendorsController.cs
+++ PierreVandO/Controllers/VendorsController.cs
@@ -7,7 +7,45 @@ namespace PierreVandO.Controllers
 		[HttpGet("/vendors")]
 		public ActionResult Index() 
 		{
+			List<Vendor> vendorList = Vendor.GetVendors();
+			return View(vendorList);
+		}
+
+		[HttpGet("/vendors/new")]
+		public ActionResult New()
+		{
 			return View();
+		}
+
+		[HttpPost("/vendors")]
+		public ActionResult Create(string name, string desc)
+		{
+			Vendor vendor = new Vendor(name, desc);
+			return RedirectToAction("Index");
+		}
+
+		[HttpGet("/vendors/{vendorId}")]
+		public ActionResult Show(int vendorId)
+		{
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			Vendor vendor = Vendor.FindVendor(id);
+			List<Order> orderList = vendor.VendorOrders;
+			model.Add("vendor", vendor);
+			model.Add("orders", orderList);
+			return View(model);
+		}
+
+		[HttpPost("/vendors/{vendorId}/orders")]
+		public ActionResult Create(int vendorId, string name, string desc, string price, string date)
+		{
+			Dictionary<string, object> model = new Dictionary<string, object>();
+			Vendor vendor = Vendor.FindVendor(id);
+			Order order = new Order(name, desc, price, date);
+			vendor.AddOrder(order);
+			List<Order> orderList = vendor.VendorOrders;
+			model.Add("vendor", vendor);
+			model.Add("orders", orderList);
+			return View("Show", model);
 		}
 	}
 }
